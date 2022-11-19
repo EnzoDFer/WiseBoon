fed7f611bb288ae60cf1f17c8d7e86e62ddb80b9
enzodfer <enzodfer@outlook.com>
2022-11-19 09:47:25 -0500

    Create BudgetModal base

---
+++ components/ui/BudgetModal/BudgetModal.tsx
@@ -0,0 +1,14 @@
+import styles from "./BudgetModal.module.css";
+
+export default function BudgetModal() {
+  return (
+    <div
+      className={styles.background}
+    >
+      <div
+        className={styles.modal}
+      >
+      </div>
+    </div>
+  )
+}
