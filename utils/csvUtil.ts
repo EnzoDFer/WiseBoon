8f8c82dc1e0c901303ece668c7cd7a7a59ac87c0
enzodfer <enzodfer@outlook.com>
2022-12-06 19:17:46 -0500

    Add expense array to csv string function

--- utils/csvUtil.ts
+++ utils/csvUtil.ts
@@ -1,6 +1,6 @@
 import { IExpense } from "../contexts/BudgetsContext";
 
-const CSV_HEADER: string = 'Budget Name,Expense Amount,Expense Description,';
+const CSV_HEADER: string= 'Budget Name,Expense Amount,Expense Description';
 
 function sortExpensesByBudget(unsortedArray: IExpense[]):IExpense[] {
   //Creates a copy of the unsorted array which it then sorts and returns
@@ -12,6 +12,22 @@ function sortExpensesByBudget(unsortedArray: IExpense[]):IExpense[] {
   });
 }
 
+function expenseArrayToCSV(expenseArray:IExpense[]):string {
+  //We first sort the expense array by the Budget Name alphabetically
+  const sortedArray: IExpense[] = sortExpensesByBudget(expenseArray);
+  //Change element structure from [{name:X,amount:Y,description:Z},etc]
+  //to [`name,amount,description`,etc]
+  const csvBodyAsArray: string[] = sortedArray.map((expense:IExpense)=>{
+    return `${expense.budgetName},${expense.amount},${expense.description}`
+  });
+  //Change structure from [`name,amount,description`,`name,amount,description`]
+  //to `name,amount,description`,\n`name,amount,description],\n`
+  const csvBodyAsString: string = csvBodyAsArray.join(',\n');
+  //final body structure is 'name,amount,description,\n,name,amount,description,\n etc'
+  //We combine it with the header
+  const completeCSV: string = CSV_HEADER + ',\n' + csvBodyAsString;
+  return completeCSV;
+}
 
 `
 Every Budget is as follows:
