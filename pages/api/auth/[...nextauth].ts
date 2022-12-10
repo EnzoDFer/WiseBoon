8963ada61d14a518edda226da1b220015dd91145
enzodfer <enzodfer@outlook.com>
2022-12-10 11:36:10 -0500

    Adding Facebook to nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,27 @@
 import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider from 'next-auth/providers/google';
+import FacebookProvider from 'next-auth/providers/facebook';
+import TwitterProvider from 'next-auth/providers/twitter';
+import Auth0Proivder from 'next-auth/providers/auth0';
+
+if (
+  !process.env.GOOGLE_ID ||
+  !process.env.GOOGLE_SECRET ||
+  !process.env.FACEBOOK_ID ||
+  !process.env.FACEBOOK_SECRET 
+) {
+  throw new Error('Error wih authentication')
+}
 
 export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
     }),
   ]
 }
