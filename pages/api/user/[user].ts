677f13e908909430a4dd2181dd61c042f268af11
enzodfer <enzodfer@outlook.com>
2022-12-31 16:29:52 -0500

    Add DELETE api functionality to user w/ auth

--- pages/api/user/[user].ts
+++ pages/api/user/[user].ts
@@ -1,6 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { unstable_getServerSession } from 'next-auth';
 import mockDB from '../../../mockDB/mockDB';
 import { filterByParam } from '../../../utils/genericHelperFuntions';
+import { IBudget, IExpense, IUser } from '../../../utils/interfaces';
+import { authOptions } from '../auth/[...nextauth]';
 
 export default async function GET(  
   req: NextApiRequest,
@@ -40,5 +43,32 @@ export async function DELETE(
   req: NextApiRequest,
   res: NextApiResponse,
   ) {
+    const session = await unstable_getServerSession(req,res, authOptions);
+    if (!session || !session.user) res.send({content:'This is a protected route.  Please log in.'})
+    // Providing API authentication for user using session
+    const userData: IUser = filterByParam(mockDB,'name',session?.user?.name,'include')[0]
+
+    const data: IBudget | IExpense = req.body;
+    if (data.hasOwnProperty('max')) { // Only budgets have 'max'
+      const updatedUserData: IBudget[] = filterByParam(userData.budgets,'id',data.id,'exclude')
+      mockDB.filter((user:IUser)=> {
+        if (user.email===session!.user!.email) {
+          return {
+            ...user,
+            budgets: updatedUserData
+          }
+        }
+      })
+    } else {
+      const updatedUserData: IExpense[] = filterByParam(userData.expenses,'id',data.id,'exclude');
+      mockDB.filter((user:IUser)=> {
+        if (user.email===session!.user!.email) {
+          return {
+            ...user,
+            expenses: updatedUserData
+          }
+        }
+      })
+    }
 
 };
