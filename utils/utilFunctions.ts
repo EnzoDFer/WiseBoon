f04a8626e6c1c338c642f79c55364d1875d288eb
enzodfer <enzodfer@outlook.com>
2023-02-12 13:13:05 -0500

    Deprecate findNameInArray
    Refactor instances of findNameInArray to instead use filterByParam

--- utils/utilFunctions.ts
+++ utils/utilFunctions.ts
@@ -15,15 +15,6 @@ export function filterByParam<T, K>(myArray:T[],param: keyof T,value:K,filter: '
   }
 }
 
-export function findNameInArray<T extends {name:string}>(myArray:T[],name: string):boolean {
-  return myArray.reduce((isPresent:boolean,currentElement:T)=>{
-    if (currentElement.name.toLowerCase()!==name.toLowerCase()) {
-      return false;
-    }
-    return true;
-  },false)
-}
-
 export function removeRedundantBreaks(str: string): string {
   //First trims starting and ending spaces
   //then replaces inner line breaks using regex with a normal space
