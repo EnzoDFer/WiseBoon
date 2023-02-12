ccce2a6131f3771699a56356fde31ce4fac61e14
enzodfer <enzodfer@outlook.com>
2023-02-12 12:30:28 -0500

    Create tests for filterByParam
    Create tests to check if filterByParam filters correctly for [param]: key with 'include' and 'exclude.

--- __tests__/utilFunctions.test.ts
+++ __tests__/utilFunctions.test.ts
@@ -13,6 +13,28 @@ describe('filterByParam', () => {
     const array2 = filterByParam(array1,'age',25,'include');
     expect(array2).not.toBe(array1);
   })
+
+  test('Returns new object array where [param]===key', () => {
+    const array1 = [
+      { name: "John", age: 25 },
+      { name: "Jane", age: 30 },
+      { name: "Jim", age: 35 },
+      { name: "Jerry", age: 25 },
+    ];
+    const array2 = filterByParam(array1,'age',25,'include');
+    expect(array2).toEqual([{ name: "John", age: 25 }, { name: "Jerry", age: 25 }]);
+  })
+
+  test('Returns new object array where [param]!==key', () => {
+    const array1 = [
+      { name: "John", age: 25 },
+      { name: "Jane", age: 30 },
+      { name: "Jim", age: 35 },
+      { name: "Jerry", age: 25 },
+    ];
+    const array2 = filterByParam(array1,'age',25,'exclude');
+    expect(array2).toEqual([{ name: "Jane", age: 30 }, { name: "Jim", age: 35 }]);
+  })
 });
 
 export {};
