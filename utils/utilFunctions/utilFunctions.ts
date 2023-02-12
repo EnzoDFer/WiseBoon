0a7f26b80267472ab586ac2924d912ef7b5f0a80
enzodfer <enzodfer@outlook.com>
2023-02-12 11:50:14 -0500

    Create documentation for filterByParam

--- utils/utilFunctions/utilFunctions.ts
+++ utils/utilFunctions/utilFunctions.ts
@@ -1,3 +1,12 @@
+/**
+ * Filters an array of objects based on the value of a specified parameter and returns a new filtered array.
+ * The filter type determines whether to include or exclude objects based on the equality of the specified parameter and value.
+ * @param myArray The array to filter by the parameter and value
+ * @param param The object parameter to filter by in the parameter array, myArray
+ * @param value The value to filter for using the object key [param].
+ * @param filter The filter type, 'include' or 'exclude'.
+ * @returns Returns a new filtered array of objects.
+ */
 export function filterByParam<T, K>(myArray:T[],param: keyof T,value:K,filter: 'include'|'exclude'):T[] {
   if (filter==='include') {
     return myArray.filter((ele:T) => ele[param]===value);
