b3ea4a4b27ce7d0fa69a4d48d9a76c924695f20f
enzodfer <enzodfer@outlook.com>
2023-02-12 12:07:46 -0500

    Add test for filterByParam
    Create test to test that the original array is not mutated.

--- utils/utilFunctions/utilFunctions.test.ts
+++ utils/utilFunctions/utilFunctions.test.ts
@@ -3,8 +3,16 @@ import { filterByParam } from "./utilFunctions";
   Testing function filterByParam
 */
 describe('filterByParam', () => {
-  test('')
+  test('Does not mutate original array', () => {
+    const array1 = [
+      { name: "John", age: 25 },
+      { name: "Jane", age: 30 },
+      { name: "Jim", age: 35 },
+      { name: "Jerry", age: 25 },
+    ];
+    const array2 = filterByParam(array1,'age',25,'include');
+    expect(array2).not.toBe(array1);
+  })
 });
-
 
 export {};
