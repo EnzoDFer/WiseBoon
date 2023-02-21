db805169d7aedbd86402c7227f21dbbedf17a1d1
enzodfer <enzodfer@outlook.com>
2023-02-20 22:03:58 -0500

    Replace placeholder in SummaryCard Replace the placeholder for total expenses

--- components/ui/SummaryCard/SummaryCard.tsx
+++ components/ui/SummaryCard/SummaryCard.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { useBudget } from '../../../contexts/BudgetsContext'
 import styles from './SummaryCard.module.scss'
 
 export const SummaryCard = () => {
+
+  const {expenses} = useBudget();
+
   return (
     <section
       className={styles.summary}
     >
       <p  className={styles.sumValue} ><span>$100</span><span> spent</span></p>
-      <p  className={styles.sumValue} ><span>3</span><span> total expenses</span></p>
+      <p  className={styles.sumValue} ><span>{expenses.length}</span><span> total expenses</span></p>
       <p  className={styles.sumValue} ><span>2</span><span> budgets over limit</span></p>
     </section>
   )
