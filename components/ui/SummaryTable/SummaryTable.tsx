882bd508cef352f428fb289ecd3bb5243d7256d3
enzodfer <enzodfer@outlook.com>
2023-03-01 18:48:28 -0500

    Create SummaryTable boiler

---
+++ components/ui/SummaryTable/SummaryTable.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./SummaryTable.module.scss"
+
+export const SummaryTable = () => {
+  return (
+    <table
+      className={styles.table}
+    >
+
+    </table>
+  )
+}
+
