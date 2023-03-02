0279aa4cbffb9e8f38206981e7bb9ce804d187c6
enzodfer <enzodfer@outlook.com>
2023-03-01 19:03:32 -0500

    Create HTML boiler for table in SummaryTable

--- components/ui/SummaryTable/SummaryTable.tsx
+++ components/ui/SummaryTable/SummaryTable.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import { useBudget } from '../../../contexts/BudgetsContext'
+import { IBudget } from '../../../utils/interfaces';
 import styles from "./SummaryTable.module.scss"
 
 export const SummaryTable = () => {
+  const { budgets } = useBudget();
   return (
     <table
       className={styles.table}
     >
-
+      <thead>
+        <th>Budget</th>
+        <th>Balance</th>
+      </thead>
+      <tbody>
+        {
+          budgets.map( (budget: IBudget, index: number) => {
+            return (
+              <tr key={`budget ${index}`}>
+                <td>{budget.name}</td>
+                <td>{}</td>      
+              </tr>
+            );
+          })
+        }
+      </tbody>
+      <tfoot/>
     </table>
   )
 }
