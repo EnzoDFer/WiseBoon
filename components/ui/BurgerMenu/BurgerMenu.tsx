10bd20a9fab250972749b7ce78bbe28c03e31eda
enzodfer <enzodfer@outlook.com>
2023-03-18 10:01:25 -0400

    Add label to burger menu for accessibility

--- components/ui/BurgerMenu/BurgerMenu.tsx
+++ components/ui/BurgerMenu/BurgerMenu.tsx
@@ -3,7 +3,8 @@ import styles from "./BurgerMenu.module.scss"
 export const BurgerMenu = () => {
   return (
     <div className={styles.burgerMenu}>
-      <input className={styles.checkbox} type='checkbox'/>
+      <label htmlFor="burgerMenu" className="hidden">Open Menu Button</label>
+      <input className={styles.checkbox} type='checkbox' id='burgerMenu' tabIndex={0}/>
       <div className={styles.bun+ ' ' + styles.bun1}/>
       <div className={styles.bun+ ' ' + styles.bun2}/>
       <div className={styles.bun+ ' ' + styles.bun3}/>
